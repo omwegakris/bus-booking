4e41fd68d255d542488f3016a28b0513166b5c3c
OmwegaChris <omwegachris@gmail.com>
2023-03-17 22:10:26 +0300

    formating a string

--- vari.py
+++ vari.py
@@ -3,6 +3,8 @@ length = len(course)
 
 first_name = "Chris"
 last_Name = "Omwega"
+sir_name = "Omwando"
 
-full_name = first_name + " " + last_Name
+full_name = f"{first_name} {last_Name} {sir_name}"
+
 print(full_name)
