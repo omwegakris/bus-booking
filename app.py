2d02d716a6f7bb9f32c8f85b3b49b70107d8e578
OmwegaChris <omwegachris@gmail.com>
2023-03-20 22:30:41 +0300

    unpacking lists

--- app.py
+++ app.py
@@ -14,3 +14,17 @@ def multiply(*numbers):
 
 
 multiply(4, 5, 6, 7, 8)
+
+
+def save_user(**user):
+    print(user["name"])
+    print(user["id"])
+    print(user["age"])
+
+
+save_user(id=5451, name="Omwega", age="14")
+
+letters = ["a", "b", "c", "d", "e", "f"]
+
+letters[0] = "A"
+print(letters)
